8d9fcefee4939f3a3be23787fc8b71bc9022001e
Ocomic <108867917+Ocomic@users.noreply.github.com>
2022-08-24 17:17:25 +0200

    change card picture
    - card can be turned and picture is changed
    - fixed some bugs

--- Mini Cards/Assets/The Game/Scripts/Card.cs
+++ Mini Cards/Assets/The Game/Scripts/Card.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Playables;
 
 public class Card : MonoBehaviour
 {
+    //image of turned card
+    public Sprite cardpicture;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +21,31 @@ public class Card : MonoBehaviour
     {
         
     }
+    private bool CardTurned()
+    {
+        return GetComponent<Image>().sprite == cardpicture;
+    }
+
+    private bool StartTurning()
+    {
+        return GetComponent<PlayableDirector>().state == PlayState.Playing;
+    }
     public void OnClick()
     {
+        if (CardTurned() || StartTurning()) { return; }
+
         Debug.Log("OnClick works");
+
+        GetComponent<PlayableDirector>().Play();
+                
+    }
+
+    public void ChangePicture()
+    {
+        if (Application.isPlaying)
+        {
+            Debug.Log("change picture");
+            GetComponent<Image>().sprite = cardpicture;
+        }
     }
 }
