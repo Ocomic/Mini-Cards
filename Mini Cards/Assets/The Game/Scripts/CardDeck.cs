1b634ec59e5cfc6aebe025205d3aaca368868646
Ocomic <108867917+Ocomic@users.noreply.github.com>
2022-08-26 16:01:35 +0200

    get card from stack
    - stack cards get visible now when selected
    - deactivated turn card, when clicked
    - disblay card only when card from stack is selected

--- Mini Cards/Assets/The Game/Scripts/CardDeck.cs
+++ Mini Cards/Assets/The Game/Scripts/CardDeck.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class CardDeck : MonoBehaviour
 {
     //shows carddeck and contains rules for interaction
     public List<CardDescription> cards;
+    public Image cardDisplay;
     public void ClickOnDeck()
     {
         Debug.Log("deck clicked");
@@ -24,6 +26,11 @@ public class CardDeck : MonoBehaviour
         Debug.Log("picked card =" + pickedCard.cardpicture + "of position=" + randomStackPosition);
 
         cards.Remove(pickedCard);
+
+        cardDisplay.sprite = pickedCard.cardpicture;
+        cardDisplay.gameObject.SetActive(true);
+
+        if (cards.Count == 0) gameObject.SetActive(false);
     }
 
 }
